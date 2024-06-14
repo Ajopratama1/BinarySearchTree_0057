0d7ab571b591f548df286fb2856b64891a7fd335
Ajopratama1 <a.ajopratama01@gmail.com>
2024-06-14 14:30:32 +0700

    memberikan informasi yang jelas bahwa perubahan tersebut melibatkan penambahan metode traversal postorder dalam kelas BinaryTree

--- BinarySearchTree_0057/BinarySearchTree_0057.cpp
+++ BinarySearchTree_0057/BinarySearchTree_0057.cpp
@@ -81,4 +81,16 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+
+	void postorder(Node* ptr) {
+		if (ROOT == NULL) {
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << "";
+		}
+	}
 };
